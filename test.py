41440158ed7daa5b1bbe8b1c190c5a92914aa7be
Park Chan yong <cksdyd54321@gmail.com>
2026-04-21 12:14:32 +0900

    python 저장 테스트

---
+++ test.py
@@ -0,0 +1,26 @@
+print("와우")
+num = 10
+num2 = 30
+result = num + num2
+print(result) 
+name = "이순신"
+print(name)
+job="군인"
+print ( name + job + " 입니다.")
+print ("ㅋ" * 5)
+반지름 = 5
+pi = 3.14
+print( 반지름 *2*pi)
+is_student = True
+is_food =False
+print(is_student)
+print(type(pi))
+num = input("정수입력 : ")
+num = int(num)
+print(num + 10)
+#한줄 주석이다
+""" 여러줄 주석이다 """
+# 문제 너비와 높이를 입력받아 사각형의 넓이를 출력하세요.
+width =int(input("너비 : "))
+height =int(input("높이 : "))
+print("넓이 : " + str(width*height))
